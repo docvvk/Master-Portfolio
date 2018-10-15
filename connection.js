1231d6bfe595c484b7fc9ff24755f2d3625d2b1f
Vivek Malhan <vivek.malhan@yahoo.com>
2018-10-15 13:54:51 -0400

    updating

--- connection.js
+++ connection.js
@@ -1,20 +1,3 @@
-// // module.exports = function() {
-//     var express = require("express");
-    
-//     var app = express();
-//     var PORT = process.env.PORT || 8080;
-    
-//     app.use(express.static('./public'));
-
-//     app.get('/', function (req, res) {
-//       res.send('hello world')
-//     })
-    
-//     app.listen(PORT, () => {
-//       console.log(`App is listening on PORT: ${PORT}`)
-//     })
-// }
-
 // Dependencies
 // =============================================================
 var express = require("express");
@@ -31,10 +14,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(express.static('./public'));
-
-// app.get('/', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'index.html'));
-// });
 
 app.listen(PORT, () => {
   console.log(`App is listening on PORT: ${PORT}`)
